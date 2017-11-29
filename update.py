c083c8ad2ee1e235898ae158642ed1c04ad45e43
xxw <angryxxw.bccto.me>
2017-11-29 21:44:17 +0800

    yes

--- update.py
+++ update.py
@@ -5,6 +5,6 @@ import os
 fname = 'index.html'
 yes_or_no = sys.argv[1][:-1] # always have one strange trailing byte
 now = datetime.datetime.now()
-line = '<p>%s-%s-%s: %s\n</p>' % (now.year, now.month, now.day, yes_or_no)
+line = '<p>%s-%s-%s: %s</p>\n' % (now.year, now.month, now.day, yes_or_no)
 with open(fname, 'a') as f:
   f.write(line)
